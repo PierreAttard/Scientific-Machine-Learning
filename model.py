d4c93128552fa1751d0525d5322dd9bc1822b0ea
PierreAttard <attard.pierre@gmail.com>
2021-10-23 22:03:25 +0200

    Add normalization Optimizer Adam with clipvalue 0.5 to avoit "Exlpoding gradient"

--- model.py
+++ model.py
@@ -2,14 +2,16 @@ import numpy as np
 from tensorflow.keras.layers import RNN, Layer
 from tensorflow.keras import Sequential
 from tensorflow.keras.optimizers import RMSprop
-from tensorflow import float32, concat, convert_to_tensor, linalg
+from tensorflow.keras import optimizers
+from tensorflow import float32, concat, convert_to_tensor, linalg, math
+import tensorflow as tf
 
 
 class RungeKuttaIntegratorCell(Layer):
     def __init__(self, omega, alpha, dt, initial_state, **kwargs):
         super(RungeKuttaIntegratorCell, self).__init__(**kwargs)
-        self._omega = omega
-        self._alpha = alpha
+        self._omega = convert_to_tensor(omega)
+        self._alpha = convert_to_tensor(alpha)
         self.initial_state = initial_state
         self.state_size = 2
         self.A = np.array([0., 0.5, 0.5, 1.0], dtype='float32')
@@ -29,12 +31,8 @@ class RungeKuttaIntegratorCell(Layer):
         :return: new_inputs, new_states
         """
         alpha = self.kernel
-        # print(states)
-        # print(states[0].shape)
-        # print(states[0])
-        # print(states[0][0][1])
-        theta = states[0][0][0]
-        theta_dot = states[0][0][1]
+        theta = states[0][:, :1]
+        theta_dot = states[0][:, 1:]
 
         theta_ddoti = self._fun(self._omega, alpha, inputs, theta, theta_dot)
         thetai = theta + self.A[0] * theta_dot * self.dt
@@ -60,29 +58,21 @@ class RungeKuttaIntegratorCell(Layer):
         :param theta_dot: pendulum angle speed
         :return:
         """
-        print("===inputs===")
-        print(inputs)
-        print(type(inputs))
-        print(inputs.shape)
-        print("===alpha===")
-        print(alpha)
-        print(type(alpha))
-        print(alpha.shape)
-        print("===omega===")
-        print(omega)
-        print(type(omega))
-        print(omega.shape)
-        return inputs - (omega ** 2) * np.sin(theta) - alpha * theta_dot
+        return inputs - math.pow(omega, tf.constant([2], dtype="float32")) * math.sin(theta) - alpha * theta_dot
 
     def get_initial_state(self, inputs=None, batch_size=None, dtype=None):
         return self.initial_state
 
 
-def create_model(omega, alpha, dt, initial_state, batch_input_shape, return_sequences=True, unroll=False):
+def create_model(omega, alpha, dt, initial_state, ub, lb, batch_input_shape, return_sequences=True, unroll=False):
     rkCell = RungeKuttaIntegratorCell(omega=omega, alpha=alpha, dt=dt, initial_state=initial_state)
     PINN = RNN(cell=rkCell, batch_input_shape=batch_input_shape, return_sequences=return_sequences, return_state=False,
                unroll=unroll)
     model = Sequential()
+    model.add(tf.keras.layers.Lambda(lambda X: 2*(X - lb)/(ub - lb) - 1))
     model.add(PINN)
-    model.compile(loss='mse', optimizer=RMSprop(1e4), metrics=['mae'])
+    optimizer = optimizers.Adam(clipvalue=0.5)
+    model.compile(loss='mse', optimizer=optimizer, metrics=['mae'])
+    # model.compile(loss='mse', optimizer=RMSprop(1e4), metrics=['mae'])
+    # model.compile(loss='mse', optimizer="adams", metrics=['mae'])
     return model
